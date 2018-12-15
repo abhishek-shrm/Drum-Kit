9366c82da2409a51fbec674753e835fa90fdc85c
ABHISHEK SHARMA <abhishek.shrm.2195@gmail.com>
2018-12-16 01:42:33 +0530

    added comments

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-window.addEventListener('keydown',function(e){
+//plays the beat
+function playingBeat(e){
    const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`);
    const key=document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if(!audio)return;
     else{
-        audio.currentTime=0;
+        audio.currentTime=0;//for repititive keystorkes
         audio.play();
         key.classList.add('playing');}
-    console.log(audio);
-});
+}
 
+//adds playing class to each keystroked beat
+window.addEventListener('keydown',playingBeat);
+
+//removes playing class from each played beat
 function removeTransition(e){
     if(e.propertyName!=='transform') return;
     this.classList.remove('playing'); 
 }
 
 const keys=document.querySelectorAll('.key');
+//checks for the keys which are pressed
 keys.forEach(key=>key.addEventListener('transitionend',removeTransition));
